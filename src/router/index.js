d5f80256e0916b5e5e6c47cd201d604214542047
Thibaut Goelff <thib-g@users.noreply.github.com>
2024-01-29 23:03:09 +0100

    Upgrade to vue 3

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,5 @@
-import Vue from 'vue';
-import VueRouter from 'vue-router';
-import HomeView from '@/views/HomeView.vue';
-
-Vue.use(VueRouter);
+import { createRouter, createWebHistory } from 'vue-router';
+import HomeView from '../views/HomeView.vue';
 
 const routes = [
   {
@@ -23,9 +20,8 @@ const routes = [
   },
 ];
 
-const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
   routes,
 });
 
