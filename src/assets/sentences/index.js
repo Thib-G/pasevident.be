22bd12cdca6d4505f5b042918e1fe366a2f69c93
Thibaut Goelff <thibaut.g@gmail.com>
2024-02-12 09:31:15 +0100

    Update index.js

--- src/assets/sentences/index.js
+++ src/assets/sentences/index.js
@@ -67,4 +67,5 @@ export default [
   { id: 61, html: 'J’suis statutaire,<br> on peut rien me faire&#8239;!' },
   { id: 62, html: 'On a beau être génial,<br> on peut pas penser à tout&#8239;!' },
   { id: 63, html: 'Ça va&#8239;?<br>Mieux que le temps&#8239;!' },
+  { id: 64, html: 'Là où le chemin de fer commence, la logique s’arrête.' },
 ];
